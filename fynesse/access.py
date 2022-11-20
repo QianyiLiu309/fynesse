cfd5ea3da56b7168b887765ac5563e05ba13fbda
QianyiLiu309 <liuqianyi0023@gmail.com>
2022-11-20 15:23:04 +0000

    Add code to initialze db

--- fynesse/access.py
+++ fynesse/access.py
@@ -7,6 +7,9 @@ import tables
 import mongodb
 import sqlite"""
 
+import pymysql
+from pymysql.constants import CLIENT
+
 # This file accesses the data
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
@@ -15,3 +18,36 @@ def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
     raise NotImplementedError
 
+def create_connection(user, password, host, database, port=3306):
+    """ Create a database connection to the MariaDB database
+        specified by the host url and database name.
+    :param user: username
+    :param password: password
+    :param host: host url
+    :param database: database
+    :param port: port number
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = pymysql.connect(user=user,
+                               passwd=password,
+                               host=host,
+                               port=port,
+                               local_infile=1,
+                               db=database,
+                               client_flag=CLIENT.MULTI_STATEMENTS
+                               )
+    except Exception as e:
+        print(f"Error connecting to the MariaDB Server: {e}")
+    return conn
+
+def initialize_database(conn, db_name):
+  cur = conn.cursor()
+  cur.execute("""
+    SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+    SET time_zone = "+00:00";
+    CREATE DATABASE IF NOT EXISTS `{db_name}` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+  """)
+  conn.commit()
+  print(f"Database {db_name} initialized")
