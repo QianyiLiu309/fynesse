0e5325ae891b81ced6f61c7b45f9b82371baaf01
QianyiLiu309 <liuqianyi0023@gmail.com>
2022-11-20 19:31:14 +0000

    Load postcode data

--- fynesse/access.py
+++ fynesse/access.py
@@ -10,6 +10,9 @@ import sqlite"""
 import pymysql
 from pymysql.constants import CLIENT
 import urllib.request
+import zipfile
+import requests
+
 
 # This file accesses the data
 
@@ -53,34 +56,6 @@ def initialize_database(conn, db_name):
   """)
   conn.commit()
   print(f"Database {db_name} initialized")
-
-
-def initialize_pp_data_schema(conn, table_name='pp_data'):
-  cur = conn.cursor()
-  cur.execute(f"""
-    DROP TABLE IF EXISTS `{table_name}`;
-    CREATE TABLE IF NOT EXISTS `{table_name}` (
-      `transaction_unique_identifier` tinytext COLLATE utf8_bin NOT NULL,
-      `price` int(10) unsigned NOT NULL,
-      `date_of_transfer` date NOT NULL,
-      `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
-      `property_type` varchar(1) COLLATE utf8_bin NOT NULL,
-      `new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,
-      `tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,
-      `primary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
-      `secondary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
-      `street` tinytext COLLATE utf8_bin NOT NULL,
-      `locality` tinytext COLLATE utf8_bin NOT NULL,
-      `town_city` tinytext COLLATE utf8_bin NOT NULL,
-      `district` tinytext COLLATE utf8_bin NOT NULL,
-      `county` tinytext COLLATE utf8_bin NOT NULL,
-      `ppd_category_type` varchar(2) COLLATE utf8_bin NOT NULL,
-      `record_status` varchar(2) COLLATE utf8_bin NOT NULL,
-      `db_id` bigint(20) unsigned NOT NULL
-    ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
-  """)
-  conn.commit()
-  print(f"Schema for table {table_name} initialized")
 
 
 def add_primary_key(conn, table_name, column_name):
@@ -136,3 +111,36 @@ def load_pp_data(conn, table_name, start_year=1995, end_year=2022):
   assert start_year <= end_year
   for year in range(start_year, end_year + 1):
     load_pp_data_single_year(conn, table_name, year)
+
+
+def create_index(conn, table_name, index_name, column_name):
+  cur = conn.cursor()
+  cur.execute(f"""
+    CREATE INDEX `{index_name}` USING HASH
+    ON `{table_name}`
+      ({column_name});
+  """)
+  conn.commit()
+  print(f"Index {index_name} created on column {column_name} in table {table_name}")
+
+
+def download_postcode_data():
+  postcode_data_url = 'https://www.getthedata.com/downloads/open_postcode_geo.csv.zip'
+  r = requests.get(postcode_data_url)
+
+  with open('open_postcode_geo.csv.zip', 'wb') as outfile:
+      outfile.write(r.content)
+
+  with zipfile.ZipFile('open_postcode_geo.csv.zip', 'r') as zip_ref:
+      zip_ref.extractall('open_postcode_geo.csv')
+
+
+def load_postcode_data(conn, table_name='postcode_data'):
+  cur = conn.cursor()
+  cur.execute(f"""
+    LOAD DATA LOCAL INFILE 'open_postcode_geo.csv' INTO TABLE `{table_name}`
+    FIELDS TERMINATED BY ',' 
+    LINES STARTING BY '' TERMINATED BY '\n';
+  """)
+  conn.commit()
+  print(f"postcode_data loaded")
