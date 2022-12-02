16dd74c2871740a6c4c5dd630dec0c4f7fda1d03
QianyiLiu309 <liuqianyi0023@gmail.com>
2022-12-02 08:08:17 +0000

    Add address

--- fynesse/address.py
+++ fynesse/address.py
@@ -1,17 +1,190 @@
 # This file contains code for suporting addressing questions in the data
 
-"""# Here are some of the imports we might expect 
-import sklearn.model_selection  as ms
-import sklearn.linear_model as lm
-import sklearn.svm as svm
-import sklearn.naive_bayes as naive_bayes
-import sklearn.tree as tree
-
-import GPy
-import torch
-import tensorflow as tf
-
-# Or if it's a statistical analysis
-import scipy.stats"""
+import datetime
+import statsmodels.api as sm
+from address import (
+    get_joined_transactions,
+    download_POI_for_feature_list,
+    calculate_features,
+)
+import pandas as pd
 
 """Address a particular question that arises from the data"""
+
+
+def poisson_regression_predict(df, design, observed_df):
+    """Fit a poisson regression model with lasso regularization using feature `design`.
+    Then use the model to predict housing price based on observed features `observed_df`
+    :param df: a dataframe containing column `price`
+    :param design: a dataframe representing the design matrix
+    :param observed_df: a dataframe for observed features
+    :return: the predicted housing price as a float
+    """
+    m_linear_basis = sm.GLM(
+        df["price"].to_numpy(), design, family=sm.families.Poisson()
+    )
+    results_basis = m_linear_basis.fit_regularized(alpha=0.05, L1_wt=1)
+    results_basis = m_linear_basis.fit()
+
+    print("Feature weights: ", results_basis.params)
+
+    return results_basis.predict(
+        observed_df.drop(["lattitude", "longitude"], axis=1).values
+    )[0]
+
+
+def gaussian_regression_predict(df, design, observed_df):
+    """Fit a gaussian regression model using feature `design`.
+    Then use the model to predict housing price based on observed features `observed_df`
+    :param df: a dataframe containing column `price`
+    :param design: a dataframe representing the design matrix
+    :param observed_df: a dataframe for observed features
+    :return: the predicted housing price as a float
+    """
+    m_linear_basis = sm.GLM(
+        df["price"].to_numpy(), design, family=sm.families.Gaussian()
+    )
+    results_basis = m_linear_basis.fit()
+
+    print("Feature weights: ", results_basis.params)
+
+    return results_basis.predict(
+        observed_df.drop(["lattitude", "longitude"], axis=1).values
+    )[0]
+
+
+def predict_price(
+    conn,
+    latitude,
+    longitude,
+    date,
+    property_type,
+    args,
+    model_name="poisson",
+    debug_mod=True,
+    df_filter=None,
+):
+    """Retrieve the joined transaction records from database and construct a
+    training set of housing prices at the specified location with dynamically
+    adjusted bbox size. Fit the specified GLM, then predict the price for the house with
+    `property_type` at certain date at the specified location.
+    :param conn: a connection to the database
+    :param latitude: latitude
+    :param longitude: longitude
+    :param date: a datetime object
+    :param args: a dict representing hyperparameters
+    :param model_name: the name of the GLM to be used
+    :param debug_mode: a boolean, only print features when set to true
+    :param df_filter: a function to filter out validation entry from the training set
+    :return: a flaot value representing the predicted housing price
+    """
+    start_date = date - datetime.timedelta(days=args["time_range"] // 2)
+    end_date = date + datetime.timedelta(days=args["time_range"] // 2)
+    print(f"Considering time range: {str(start_date)} to {str(end_date)}")
+
+    bbox_size = args["bbox_size_initial"]
+    required_sample_size = args["required_sample_size"]
+    features = args["features"]
+
+    while True:
+        transaction_df = get_joined_transactions(
+            conn,
+            start_date,
+            end_date,
+            latitude,
+            longitude,
+            bbox_size,
+            bbox_size,
+            property_type,
+        )
+        if len(transaction_df) < required_sample_size:
+            bbox_size *= args["increase_factor"]
+            if bbox_size > args["bbox_size_limit"]:
+                print(
+                    f"The number of samples is {len(transaction_df)}, which is less than the requirement {required_sample_size}. Consider properties of all types. Prediction results may be inaccurate in this case."
+                )
+                break
+        else:
+            break
+
+    # Build up the training set from housing of all types
+    if len(transaction_df) < required_sample_size:
+        transaction_df = get_joined_transactions(
+            conn, latitude, longitude, bbox_size, bbox_size, start_date, end_date
+        )
+
+    print(f"Retrieved {len(transaction_df)} transactions with bbox size = {bbox_size}")
+
+    if len(transaction_df) == 0:
+        return "No samples in the training set"
+
+    # used to filter out validation entry during evaluation
+    if df_filter is not None:
+        transaction_df = df_filter(transaction_df)
+
+    # bbox for features
+    feature_bbox_size = bbox_size * 2
+    dist_threshold = bbox_size / 2
+
+    df = transaction_df[["price", "lattitude", "longitude"]].copy()
+    pois_map = download_POI_for_feature_list(
+        latitude, longitude, bbox_size, bbox_size, features
+    )
+    df = calculate_features(df, features, dist_threshold, pois_map)
+    df["one"] = 10
+    if debug_mod:
+        print("Features: ", df)
+
+    observed_df = pd.DataFrame({"lattitude": [latitude], "longitude": [longitude]})
+    observed_df = calculate_features(observed_df, features, dist_threshold, pois_map)
+    observed_df["one"] = 10
+    if debug_mod:
+        print("Observed features: ", observed_df)
+
+    design = df.drop(["price", "lattitude", "longitude"], axis=1).values
+
+    if model_name == "poisson":
+        return poisson_regression_predict(df, design, observed_df)
+    elif model_name == "gaussian":
+        return gaussian_regression_predict(df, design, observed_df)
+    else:
+        raise NotImplementedError
+
+
+def filter_out_validation_data(transaction_df, val_db_id):
+    """Filter out the record specified by val_df_id from the training dataset
+    :param transaction_df: the training set of price data
+    :param val_db_id: the db_id of the validation data point
+    :return: a dataframe, with one entry removed from transaction_df
+    """
+    filtered_df = transaction_df[transaction_df.db_id != val_db_id]
+    if len(filtered_df) < len(transaction_df):
+        print(f"Successfully removed the entry with db_id: {val_db_id}")
+    return filtered_df
+
+
+def evaluate_model(conn, validation_df, model_name, args):
+    """Evaluate the specified model using the known price of a property
+    : conn
+
+    """
+    real_price_ls = []
+    predicted_price_ls = []
+    for _, row in validation_df.iterrows():
+        val_db_id = row.db_id
+        real_price = row.price
+        filter_fn = lambda df: filter_out_validation_data(df, val_db_id)
+        predicted_price = predict_price(
+            conn,
+            row.lattitude,
+            row.longitude,
+            row.date_of_transfer,
+            row.property_type,
+            args,
+            model_name,
+            debug_mod=False,
+            df_filter=filter_fn,
+        )
+        real_price_ls.append(real_price)
+        predicted_price_ls.append(predicted_price)
+    return real_price_ls, predicted_price_ls
--- fynesse/assess.py
+++ fynesse/assess.py
@@ -1,48 +1,22 @@
 from .config import *
-
 from fynesse import access
+
 import mlai
 import mlai.plot as plot
-import pandas as pd
-import math
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 import geopandas as gpd
-
-import matplotlib.pyplot as plt
-
 import osmnx as ox
+
+
 import math
 import random
-
-"""These are the types of import we might expect in this file
-import pandas
-import bokeh
-import seaborn
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-import sklearn.decomposition as decomposition
-import sklearn.feature_extraction"""
+
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
-
-
-# def data():
-#     """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
-#     df = access.data()
-#     raise NotImplementedError
-
-
-# def query(data):
-#     """Request user input for some aspect of the data."""
-#     raise NotImplementedError
-
-
-# def view(data):
-#     """Provide a view of the data that allows the user to verify some aspect of its quality."""
-#     raise NotImplementedError
-
-
-# def labelled(data):
-#     """Provide a labelled set of data ready for supervised learning."""
-#     raise NotImplementedError
 
 
 def verify_database(conn):
@@ -75,7 +49,44 @@ def verify_table_content(conn, table_name):
     return df
 
 
+def get_average_housing_price_per_year(
+    conn,
+    per_property_type=False,
+    pp_data="pp_data",
+    date="date_of_transfer",
+    type="property_type",
+):
+    """Query the average housing price per year
+    :param conn: a connection to database
+    :param per_property_type: a boolean indicating whether we group by property_type
+    :param pp_data: db table name
+    :param date: column name for date
+    :param type: column name for property_type
+    :return: a dataframe containing the average housing price
+    """
+    column_names = f" avg(price) as mean_price, EXTRACT(year FROM `{date}`) as year"
+    if per_property_type:
+        column_names += f", {type} as {type}"
+    sql_query = (
+        "SELECT"
+        + column_names
+        + f" FROM `{pp_data}` GROUP BY EXTRACT(year FROM `{date}`)"
+    )
+    if per_property_type:
+        sql_query += f" , `{type}`"
+    df = pd.read_sql(sql_query, con=conn)
+    return df
+
+
 def get_average_distance_to_POI(latitude, longitude, pois, threshold):
+    """Calculate the average distance from the point (latitude, longitude) to POIs.
+    Valid distances should be less than thresdhold.
+    :param latitude: latitude
+    :param longitude: longitude
+    :param pois: a geodataframe representing Points of Interest
+    :param threshold: the upper limit of distance to be considered when calculating the mean
+    :return: a float value representing average distance to POIs
+    """
     total_dis = 0
     for row in pois.iterrows():
         polygon = row[1]["geometry"]
@@ -89,6 +100,13 @@ def get_average_distance_to_POI(latitude, longitude, pois, threshold):
 
 
 def get_cnt_of_POI(latitude, longitude, pois, threshold):
+    """Count the number of POIs within the distance of `threshold` from the point (latitude, longitude)
+    :param latitude: latitude
+    :param longitude: longitude
+    :param pois: a geodataframe representing Points of Interest
+    :param threshold: the upper limit of distance to be considered when calculating the count
+    :return: an integer value representing the number of nearby POIs
+    """
     cnt = 0
     for row in pois.iterrows():
         polygon = row[1]["geometry"]
@@ -102,6 +120,14 @@ def get_cnt_of_POI(latitude, longitude, pois, threshold):
 
 
 def get_shortest_distance_to_POI(latitude, longitude, pois, threshold):
+    """Calculate the shortest distance from the point (latitude, longitude) to any POI in pois.
+    Valid distances should be less than thresdhold.
+    :param latitude: latitude
+    :param longitude: longitude
+    :param pois: a geodataframe representing Points of Interest
+    :param threshold: the upper limit of distance to be considered when calculating the minimum
+    :return: a float value representing shortest distance to that POI
+    """
     shortest_dist = threshold
     for row in pois.iterrows():
         polygon = row[1]["geometry"]
@@ -115,6 +141,13 @@ def get_shortest_distance_to_POI(latitude, longitude, pois, threshold):
 def create_gdf_from_df(
     df, latitude_col_name="lattitude", longitude_col_name="longitude"
 ):
+    """Create a geodataframe from a dataframe, using the specified columns as
+    latitude and longitude
+    :param df: a pandas dataframe containing latitude and longitude as columns
+    :param latitude_col_name: name of the latitude column in df
+    :param longitude_col_name: name of the longitude column in df
+    :return: a geopandas dataframe
+    """
     assert latitude_col_name in df.columns.values
     assert longitude_col_name in df.columns.values
 
@@ -124,6 +157,13 @@ def create_gdf_from_df(
 
 
 def get_bbox(latitude, longitude, box_width, box_height):
+    """Create a bounding box based on the coordinate
+    :param north: north
+    :param south: south
+    :param west: west
+    :param esat: east
+    :return: a tuple (north, south, west, east) representing the bounding box
+    """
     north = latitude + box_height / 2
     south = latitude - box_height / 2
     west = longitude - box_width / 2
@@ -139,6 +179,15 @@ def download_POI_around_coordinate(
     tags={},
     columns=["name", "addr:city", "addr:postcode", "addr:street", "geometry"],
 ):
+    """Download POI as specified by tags around a coordinate
+    :param latitude: latitude
+    :param longitude: longitude
+    :param box_width: width of bbox
+    :param box_height: height of bbox
+    :param tags: a dict of POI tags
+    :param columns: a list of interested column names
+    :return: a geopandas dataframe containing points of interest
+    """
     north, south, west, east = get_bbox(latitude, longitude, box_width, box_height)
     pois = ox.geometries_from_bbox(north, south, east, west, tags)
 
@@ -160,6 +209,17 @@ def plot_POI(
     graph_name=None,
     plot_coordinate=False,
 ):
+    """Geo-plot POIs on the map for the given place specified by place_name
+    :param latitude: latitude
+    :param longitude: longitude
+    :param place_name: a geo-decodable string representing place name
+    :param box_width: width of bbox
+    :param box_height: height of bbox
+    :param pois: a list of POIs
+    :param graph_name: the name of the plot
+    :param plot_coordinate: boolean value, plot the coordinate (lat, long) if true
+    :return: None
+    """
     north, south, west, east = get_bbox(latitude, longitude, box_width, box_height)
 
     graph = ox.graph_from_bbox(north, south, east, west)
@@ -189,9 +249,17 @@ def plot_POI(
 def sample_locations_from_bbox(
     latitude, longitude, box_height, box_width, n_sample=100
 ):
+    """Get random samples of coordinates within the bounding box of the given location
+    :param latitude: latitude
+    :param longitude: longitude
+    :param box_width: width of bbox
+    :param box_height: height of bbox
+    :param n_sample: the number of required samples
+    :return: a dataframe of coordinates
+    """
     latitudes = []
     longitudes = []
-    for _ in range(100):
+    for _ in range(n_sample):
         i = random.uniform(latitude - box_height / 2, latitude + box_height / 2)
         j = random.uniform(longitude - box_width / 2, longitude + box_width / 2)
         latitudes.append(i)
@@ -202,6 +270,16 @@ def sample_locations_from_bbox(
 def calculate_single_feature(
     df, feature_fn, dist_threshold, method_name, poi, feature_name
 ):
+    """Calculate single feature specified by `feature_fn` and add it as a new
+    column with name `feature_name` to the input dataframe `df`
+    :param df: a dataframe of transactions and features
+    :param feature_fn: a function used to compute feature
+    :param dist_threshold: distance threshold
+    :param method_name: a string
+    :param poi: the POIs used to compute the feature
+    :feature_name: the type name of the POIs
+    :return: a dataframe containing the new feature
+    """
     column_name = feature_name + "_" + method_name
     df[column_name] = df.apply(
         lambda row: feature_fn(
@@ -213,6 +291,13 @@ def calculate_single_feature(
 
 
 def calculate_features(df, features, dist_threshold, pois_map):
+    """Apply 'calculate_single_feature` to a collection of features
+    :param df: a dataframe of transactions
+    :param features: a JSON encoding of features
+    :param dist_threshold: distance threshold
+    :param pois_map: a mapping from feature_name to POIs
+    :return: a dataframe containing all computed features
+    """
     for feature_name, prop in features.items():
         for method_name in prop["methods"]:
             if method_name == "cnt":
@@ -237,6 +322,14 @@ def calculate_features(df, features, dist_threshold, pois_map):
 def download_POI_for_feature_list(
     latitude, longitude, feature_box_width, feature_box_height, features
 ):
+    """Download POIs from OpenStreetMap as specified by the tags attributes in `features`
+    :param latitude: latitude
+    :param longitude: longitude
+    :param feature_box_width: width of feature bbox
+    :param feature_box_height: height of feature bbox
+    :param features: a JSON encoding of features
+    :return: a mapping from feature name to POIs
+    """
     pois_map = {}
     for name, prop in features.items():
         tag = prop["tags"]
@@ -250,3 +343,91 @@ def download_POI_for_feature_list(
         pois_map[name] = pois
         print(f"POIs for feature: {name} downloaded")
     return pois_map
+
+
+def plot_geo_transactions(df):
+    """Plot house locations as specified in `df` on UK map
+    :param df: a dataframe representing house locations
+    :return: None
+    """
+    fig, ax = plt.subplots(figsize=(12, 12))
+    area = ox.geocode_to_gdf("United Kingdom")
+    area.plot(ax=ax, facecolor="lightgray")
+
+    ax.scatter(
+        df.longitude,
+        df.lattitude,
+        c=np.log(df.price.values),
+        alpha=0.005,
+    )
+    plt.show()
+
+
+def plot_scatter_matrix_for_features(
+    features, latitude, longitude, box_width, box_height, threshold, to_plot=True
+):
+    """Download POIs for feature list, calculate features for the randomly sampled locations,
+    and then plot the scatter matrix of features if to_plot is set to True
+    :param features: a JSON encoding of features
+    :param latitude: latitude
+    :param longitude: longitude
+    :param box_width: width of bbox
+    :param box_height: height of bbox
+    :param threshold: distance threshold
+    :param to_plot: a boolean incidating whether scatter matrix should be plot
+    :return: a dataframe containing all computed features
+    """
+    pois_map = download_POI_for_feature_list(
+        latitude, longitude, box_width, box_height, features
+    )
+    df = sample_locations_from_bbox(
+        latitude, longitude, box_height, box_width, n_sample=30
+    )
+    df_with_features = calculate_features(df, features, threshold, pois_map)
+    pure_features = df_with_features.drop(["lattitude", "longitude"], axis=1)
+
+    if to_plot:
+        axes = pd.plotting.scatter_matrix(pure_features, alpha=0.2, figsize=(13, 13))
+        for ax in axes.flatten():
+            ax.xaxis.label.set_rotation(45)
+            ax.xaxis.label.set_ha("right")
+            ax.yaxis.label.set_rotation(45)
+            ax.yaxis.label.set_ha("right")
+        plt.show()
+    return df
+
+
+def pca_analysis(pure_features, num_features):
+    """Perform PCA analysis on features specified by `pure_features`
+    :param pure_features: a collection of features in a dataframe
+    :param num_featuress: total number of features
+    :return: None
+    """
+    scaler = StandardScaler()
+    scaler.fit(pure_features)
+    pure_features = scaler.transform(pure_features)
+
+    pca = PCA(n_components=num_features)
+    features_new = pca.fit_transform(pure_features)
+    exp_var_pca = pca.explained_variance_ratio_
+    cum_sum_eigenvalues = np.cumsum(exp_var_pca)
+
+    fig, ax = plt.subplots(figsize=plot.big_figsize)
+    plt.bar(
+        range(0, len(exp_var_pca)),
+        exp_var_pca,
+        alpha=0.5,
+        align="center",
+        label="Individual explained variance",
+    )
+    plt.step(
+        range(0, len(cum_sum_eigenvalues)),
+        cum_sum_eigenvalues,
+        where="mid",
+        label="Cumulative explained variance",
+    )
+    plt.ylabel("Explained variance ratio")
+    plt.xlabel("Principal component index")
+    plt.legend(loc="best")
+    plt.tight_layout()
+    plt.show()
